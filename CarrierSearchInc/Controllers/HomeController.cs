9b7d30182313beff42872aac8a3cca35585b61d5
abayomi igwubor <igwubor@outlook.com>
2020-05-27 10:32:09 +0100

    added myViewLayoutandAboutUsView

--- CarrierSearchInc/Controllers/HomeController.cs
+++ CarrierSearchInc/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CarrierSearchInc.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
