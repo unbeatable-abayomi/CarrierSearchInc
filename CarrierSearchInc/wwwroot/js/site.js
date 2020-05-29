74a80349b5c6b1b8c2f33258954dc880cba4fa58
abayomi igwubor <igwubor@outlook.com>
2020-05-29 16:10:37 +0100

    added ClientSide Validation with Javascript

--- CarrierSearchInc/wwwroot/js/site.js
+++ CarrierSearchInc/wwwroot/js/site.js
@@ -10,10 +10,7 @@ const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
 const address = document.querySelector("#address");
 
-console.log(firstName);
-console.log(lastName);
-console.log(email);
-console.log(phone);
+
 const spanFirstName = document.querySelector(".firstname");
 const spanLastName = document.querySelector(".lastname");
 const spanEmail = document.querySelector(".email");
@@ -23,6 +20,9 @@ const spanAddress = document.querySelector(".address");
 
 firstName.addEventListener("blur", ValidateFirstName);
 lastName.addEventListener("blur", ValidateLastName);
+email.addEventListener("blur", ValidateEmail);
+phone.addEventListener("blur", ValidatePhone);
+address.addEventListener("blur", ValidateAddress);
 
 //ValidateFirstName = () => {
 
@@ -52,6 +52,43 @@ function ValidateLastName() {
     }
 }
 
+function ValidateEmail() {
+
+    let emailAd = email.value;
+        let compareEmail = new RegExp('[a-z0-9_.]+@[a-z0-9-]+\.[a-z]{2,6}', "gi");
+        if (compareEmail.test(emailAd)) {
+            spanEmail.innerHTML = "Good!";
+            spanEmail.style.color = "green";
+        } else {
+            spanEmail.innerHTML = "Oops! incorrect input";
+            spanEmail.style.color = "red";
+        }
+    
+}
+
+function ValidatePhone() {
+    let phoneNumber = phone.value;
+    let comparePhone = new RegExp('[1-9]{4,15}', "gi");
+    if (comparePhone.test(phoneNumber)) {
+        spanPhone.innerHTML = "Good!";
+        spanPhone.style.color = "green";
+    } else {
+        spanPhone.innerHTML = "Oops! incorrect input";
+        spanPhone.style.color = "red";
+    }
+}
+
+function ValidateAddress() {
+    let addres = address.value;
+    let comparefName = new RegExp('[a-z]{4,100}', "gi");
+    if (comparefName.test(addres)) {
+        spanAddress.innerHTML = "Good!";
+        spanAddress.style.color = "green";
+    } else {
+        spanAddress.innerHTML = "Oops! incorrect input";
+        spanAddress.style.color = "red";
+    }
+}
 
 
 
